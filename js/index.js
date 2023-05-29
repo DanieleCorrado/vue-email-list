eb2b28e5727fc7859b1918039a9da39ce01edd3d
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-29 15:32:51 +0200

    add API mail request

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-console.log("prova");
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      emailNumber: 10,
+      mailingList: []
+    }
+  },
+
+  mounted() {
+    for(let i = 0; i < this.emailNumber; i++) {
+
+      // Richiesta all'API 
+
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(
+      answer => {
+
+        this.mailingList[i] = answer.data.response;
+        
+      }
+    )
+    }
+  }
+
+}).mount("#myapp")
